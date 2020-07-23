acdd329de7ad3272df2b8a764e39e44ef2881f57
sandeep <itissandeep98@gmail.com>
2020-07-23 21:13:34 +0530

    input field added

--- main.js
+++ main.js
@@ -3,12 +3,7 @@ window.addEventListener('load', function () {
 	new Vue({
 		el: "#app",
 		data: {
-			isSafe: true,
-		},
-		methods: {
-			demo() {
-				alert("link clicked")
-			}
+			title:""
 		}
 	})
 })
