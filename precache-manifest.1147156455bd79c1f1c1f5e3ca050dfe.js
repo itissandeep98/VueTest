1423422ab95404cdc24308eb57c5ac05e81eeea1
sandeep <itissandeep98@gmail.com>
2020-07-29 20:19:20 +0530

    Updates

---
+++ precache-manifest.1147156455bd79c1f1c1f5e3ca050dfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857ac1ed17fba28a3b74",
+    "url": "/css/app.13ba594e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ffe9f326ab145f4b8400569ede98fb4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3ee2c20fe407eefd1a4",
+    "url": "/js/about.a241b303.js"
+  },
+  {
+    "revision": "857ac1ed17fba28a3b74",
+    "url": "/js/app.16642719.js"
+  },
+  {
+    "revision": "342bcee166fa9bd5aa04",
+    "url": "/js/chunk-vendors.1fcdfd2c.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
