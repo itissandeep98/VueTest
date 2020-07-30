0031745043cbb5f803730a6dc4a3a7dd5fc6edf8
itissandeep98 <itissandeep98@gmail.com>
2020-07-31 01:39:56 +0530

    calendar added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,9 @@
 			<div class="row">
 				<div class="col-12 col-md-6 text-center">
 					<br/><br/>
-					<input type="text" v-model="inp" placeholder="Enter Your Task" @keyup.enter="addToList"/>
+					<input type="text" v-model="inp.text" placeholder="Enter Your Task"/>
+					<br/><br/>
+					<b-calendar v-model="inp.date" :date-disabled-fn="dateDisabled"/>
 					<br/><br/>
 					<b-button pill variant="secondary" @click="addToList" >
 						Add to List <span class="fas fa-plus"/>
@@ -16,10 +18,10 @@
 						<b-tab title="Active" >
 							<ul class="">
 								<li v-for="(el,ind) in activeList" :key="ind">
-									<b-button pill variant="danger" @click="markCompleted(ind)" v-b-popover.hover.left="'Mark As Completed'">
-										<span class="far fa-hand-scissors"/>
+									<b-button pill variant="info" @click="markCompleted(ind)" v-b-popover.hover.left="'Mark As Completed'">
+										<span class="far fa-check-circle"/>
 									</b-button>
-									<p  v-show="isedit!=ind" @click="isedit=ind;temp=el">{{el}}</p>
+									<p  v-show="isedit!=ind" @click="isedit=ind;temp=el">{{el.text}}  {{el.date}}</p>
 									<input type="text" v-model="temp" v-show="ind===isedit" @keyup.enter="update(ind)" />
 								</li>
 							</ul>
@@ -28,9 +30,9 @@
 							<ul class="">
 								<li v-for="(el,ind) in completedList" :key="ind">
 									<b-button pill variant="danger" @click="deleteval(ind)" v-b-popover.hover.left="'Delete Permanently'">
-										<span class="far fa-hand-scissors"/>
+										<span class="far fa-times-circle"/>
 									</b-button>
-									<p>{{el}}</p>
+									<p>{{el.text}}  {{el.date}}</p>
 								</li>
 							</ul>
 						</b-tab>
@@ -49,15 +51,20 @@ export default {
 	},
 	data(){
 		return {
-			inp:"",
+			inp:{
+				text:"",
+				date:""
+			},
 			activeList:[],
 			completedList:[],
 			isedit:-1,
-			temp:""
+			temp:"",
+			date:""
 		}
 	},
 	methods:{
 		addToList(){
+			this.inp.date=new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(this.inp.date)))
 			this.activeList.push(this.inp);
 			this.inp=""
 		},
@@ -72,6 +79,11 @@ export default {
 		},
 		deleteval(i){
 			this.completedList.splice(i,1)
+		},
+		dateDisabled(ymd, date) {
+			const day = date.getDate()
+			const d=new Date()
+			return  day < d.getDate() && d.getMonth()>=date.getMonth()
 		}
 		
 	}
